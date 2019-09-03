6d6ce1b20923790124d1efee48f36aef964ef56c
Luis Phelipe <luisphelipecode@gmail.com>
2019-09-03 03:27:17 +0000

    Add script to wake up the heroku servers

---
+++ wake-heroku-servers.js
@@ -0,0 +1,20 @@
+const serverList = [
+  "https://receipe-reciepe-recipe-app.herokuapp.com/",
+  "https://rails-graphql-todo.herokuapp.com/",
+  "https://luisphelipe-express-todo.herokuapp.com/",
+  "http://arcane-island-39787.herokuapp.com"
+];
+
+console.log(
+  "beep boop Starting the wake up process... this can take up to a minute."
+);
+
+serverList.forEach(serverUrl => {
+  fetch(serverUrl).then(() => {
+    console.log(`Sucessfuly awaked ${serverUrl}.`);
+  });
+});
+
+console.log(
+  "All servers are awake, better user experience is ensured yet again! :)"
+);
