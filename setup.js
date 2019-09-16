cf8b5e439ceec5c1be4e3aba2d1d05b52ec8986c
Luis Phelipe <luisphelipecode@gmail.com>
2019-09-16 19:44:18 +0000

    Add images and mess around with the style a bit

---
+++ setup.js
@@ -0,0 +1,27 @@
+const options = {
+  button: false,
+  title: false,
+  toolbar: false,
+  tooltip: false,
+  movable: false,
+  rotatable: false,
+  scalable: false,
+  transition: false,
+  loading: false,
+};
+
+Viewer.setDefaults(options);
+
+const galleries = [
+  'native-agenda-gallery',
+  'kanban-gallery',
+  'recipes-gallery',
+  'mern-todo-gallery',
+  'laravel-agenda-gallery',
+  'draughts-gallery',
+  'automata-gallery',
+];
+
+galleries.forEach(gallery => {
+  new Viewer(document.getElementById(gallery));
+});
